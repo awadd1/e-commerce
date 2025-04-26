0e5165f17ce7ddd858a1bf56f2b588eed5679bb3
Awadd <muhammedawadd1@gmail.com>
2025-04-26 15:45:37 +0300

    Initial Commit

---
+++ app/Http/Controllers/Backend/PaymentSettingController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\PaypalSetting;
+use App\Models\StripeSetting;
+use App\Models\CodSetting;
+use App\Models\RazorpaySetting;
+use Illuminate\Http\Request;
+
+class PaymentSettingController extends Controller
+{
+    public function index()
+    {
+       $paypalSetting   = PaypalSetting::first();
+       $stripeSetting   = StripeSetting::first();
+       $razorpaySetting = RazorpaySetting::first();
+       $codSetting      = CodSetting::first();
+
+
+       
+       return view('admin.payment-settings.index', compact('paypalSetting', 'stripeSetting', 'razorpaySetting', 'codSetting'));
+    }
+
+}
